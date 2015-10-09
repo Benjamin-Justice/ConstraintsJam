56f7591f23927dd0092509738075af5cbe9ed447
Benjamin Justice <b.justice@aleron.eu>
2015-10-10 01:18:55 +0200

    Enemy Health and basic attacks

---
+++ AsyncTD/Assets/AttackEnemiesInRange.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AttackEnemiesInRange : MonoBehaviour
+{
+	public Collider attackRange;
+	private List<EnemyHealth> enemiesInRange;
+
+	// Use this for initialization
+	void Start ()
+	{
+		enemiesInRange = new List<EnemyHealth> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		enemiesInRange.ForEach ((enemy) => enemy.Health -= 1);
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		EnemyHealth otherEnemy = other.GetComponent<EnemyHealth> ();
+		if (otherEnemy != null) {
+			enemiesInRange.Add (otherEnemy);
+			Debug.Log ("Added " + otherEnemy.gameObject.name);
+		}
+	}
+
+	void OnTriggerExit (Collider other)
+	{
+		EnemyHealth otherEnemy = other.GetComponent<EnemyHealth> ();
+		if (otherEnemy != null) {
+			enemiesInRange.Remove (otherEnemy);
+			Debug.Log ("Removed " + otherEnemy.gameObject.name);
+		}
+	}
+}
--- AsyncTD/Assets/EnemyHealth.cs
+++ AsyncTD/Assets/EnemyHealth.cs
@@ -1,18 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyHealth : MonoBehaviour {
+[RequireComponent (typeof(NavMeshAgent))]
+public class EnemyHealth : MonoBehaviour
+{
+	private int health = 100;
+	public Animator animator;
+
+	public int Health {
+		get {
+			return health;
+		}
+
+		set {
+			this.health = value;
+			if (health <= 0) {
+				if (animator != null) {
+					animator.SetBool ("dead", true);
+					this.GetComponent<NavMeshAgent> ().enabled = false;
+				} else {
+					this.onDeathAnimationComplete ();
+				}
+			}
+		}
+	}
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+		
 	}
-	public void onDeathAnimationComplete(){
-		Object.Destroy(this.gameObject);
+
+	public void onDeathAnimationComplete ()
+	{
+		Object.Destroy (this.gameObject);
 	}
 }
