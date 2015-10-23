828dc94ac63b1c503faa0f263d09b800bc67df22
Sergej Tihonov <s.tihonov@aleron.eu>
2015-10-24 01:17:56 +0200

    GUI Prefabs und Scripte muss noch in die Scene reingezogen werden.

---
+++ AsyncTD/Assets/Scripts/GUI/GameGuiManager.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameGuiManager : MonoBehaviour
+{
+	public Image playerImage;
+	public Image buildTowerPanel;
+	public Image upgradeTowerPanel;
+	public Button startButton;
+	public Text levelDescription;
+	public Image enemyImage;
+	public Text healthValue;
+	public Text moneyValue;
+	private int level = 0;
+
+	// Use this for initialization
+	void Start ()
+	{
+		buildTowerPanel.gameObject.SetActive (false);
+		upgradeTowerPanel.gameObject.SetActive (false);
+	}
+
+	public void updatePlayerImage (Sprite playerSprite)
+	{
+		playerImage.sprite = playerSprite;
+	}
+
+	public void showNextLevelGui (Sprite enemySprite)
+	{
+		level++;
+		levelDescription.text = "Next Level: " + level;
+		enemyImage.sprite = enemySprite;
+		startButton.gameObject.SetActive (true);
+	}
+
+	public void showCurrentLevelGui ()
+	{
+		levelDescription.text = "Current Level: " + level;
+		startButton.gameObject.SetActive (false);
+	}
+
+	public void showBuildTowerPanel ()
+	{
+		buildTowerPanel.gameObject.SetActive (true);
+	}
+
+	public void disableBuildTowerPanel ()
+	{
+		buildTowerPanel.gameObject.SetActive (false);
+	}
+
+	public void showUpgradeTowerPanel ()
+	{
+		upgradeTowerPanel.gameObject.SetActive (true);
+	}
+
+	public void disableUpgradeTowerPanel ()
+	{
+		upgradeTowerPanel.gameObject.SetActive (false);
+	}
+
+	public void updateHealthValue (int value)
+	{
+		healthValue.text = value.ToString ();
+	}
+
+	public void updateMoneyValue (int value)
+	{
+		moneyValue.text = value.ToString ();
+	}
+}
---
+++ AsyncTD/Assets/Scripts/GUI/StartGuiManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StartGuiManager : MonoBehaviour
+{
+	public Image[] player = new Image[6];
+
+	// Use this for initialization
+	void Start ()
+	{
+		foreach (Image image in player) {
+			image.gameObject.SetActive (false);
+		}
+	}
+
+	public void showPlayerImage (int playerArrayPosition, Sprite playerSprite)
+	{
+		player [playerArrayPosition].gameObject.SetActive (true);
+		player [playerArrayPosition].sprite = playerSprite;
+
+	}
+}
